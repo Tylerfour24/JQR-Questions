05b3de8f69fff3f4065b4d133b12696bb64f1469
Tyler Gantt <tgantt@localhost.localdomain>
2016-01-17 10:08:01 -0500

    performs basic math functions, and demonstrates appropriate use of PEMDAS

---
+++ C_arithmetic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	int math = 10;
+	int ematics = 4;
+
+	printf("%d\n", math + ematics);
+	printf("%d\n", math - ematics);
+	printf("%d\n", math * ematics);
+	printf("%d\n", math / ematics);
+	printf("%d\n", math % ematics);
+
+	printf("%lf\n", pow((((6+7)*4)/6), 2));
+	return 0;
+}
