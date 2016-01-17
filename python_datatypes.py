ce8f8f5229620e8920d0afcdfff7563a6f6a907a
Tyler Gantt <tgantt@localhost.localdomain>
2016-01-17 11:03:36 -0500

    updated to actually utilize type function

--- python_datatypes.py
+++ python_datatypes.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
-x = 10
 
-print(chr(x))
-print(float(x))
-print(int(x))
-print(hex(x))
-print(bin(x))
+integer = 19
+floating = 19.19
+string = "string"
+hexadecimal = 0x12ABC4
+binary = 10101010
+
+
+print(type(integer))
+print(type(floating))
+print(type(string))
+print(type(hexadecimal))
+print(type(binary))
